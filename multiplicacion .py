4d331b557fd2e729e9c036181f535055505f9312
yefelso <1313124@senati.pe>
2023-02-24 07:39:58 -0500

    python operacion suma y multiplicacion

--- multiplicacion .py
+++ multiplicacion .py
@@ -1,10 +1,13 @@
 n1 = 6
 n2 = 6
 print(n1*n2)
+print(" =36 ")
+
 n1 = 10
 n2 = 9
 n3 =3
 print(n1*n2*n3)
+print(" =270 ")
 
 print ("formula")
 n1 = ("primer numero")
--- suma.py
+++ suma.py
@@ -1,10 +1,13 @@
 n1 = 5
 n2 = 6
 print(n1+n2)
+print(" = 11 ")
+
 n1 = 20
 n2 = 25
 n3 = 65
 print(n1+n2+n3)
+print(" = 110 ")
 
 print("formula")
 n1 = ("primer numero")
