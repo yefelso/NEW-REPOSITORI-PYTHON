b53b0481e007992d9a7554c215c7fc71f92a9c4d
yefelso <1313124@senati.pe>
2023-02-25 08:36:32 -0500

    python numeros divisores de 5

---
+++ numeros divisores de 5.py
@@ -0,0 +1,10 @@
+def suma_divisores(5):
+suma = 0
+for i in a range(15,17,26,35,40,50,57,80,83,100)
+if 5 % i == 0:
+    suma += i
+    return suma
+
+    5 = int(imput(15,17,26,35,40,50,57,80,83,100))
+    print("la suma de sus divisores es:")
+    print(suma_divisores(5))
