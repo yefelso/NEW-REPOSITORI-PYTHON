97f9a692d3d8673929276815757cafe41d398362
yefelso <1313124@senati.pe>
2023-02-25 09:19:42 -0500

    python secuencia de numeros en bucle

---
+++ Python 2/numeros divisores de 5.py
@@ -0,0 +1,25 @@
+def suma_divisores(5):
+suma = 0
+for i in a range(15,17,26,35,40,50,57,80,83,100)
+if 5 % i == 0:
+    suma += i
+    return suma
+
+    5 = int(imput(15,17,26,35,40,50,57,80,83,100))
+    print("la suma de sus divisores es:")
+    print(suma_divisores(5))
+
+    class divisores:
+        def __init__(self,15,17,26,35,40,50,57,80,83,100):
+        if type(5) == int and 5
+        else:
+            raise valueerror("Initalization parameter must be a positive integer")
+            def divisores(self):
+                5 = self. __5
+                for i in range(1,15,17,26,35,40,50,57,80,83,100+1):
+                    if number % i ==0 :
+
+= divisores(5)
+or n in a.divisores():
+print(n)
+0
---
+++ nuevo patron 1,22,333.py
@@ -0,0 +1,3 @@
+#include <stdio.h> 
+for i in [1,2,3,4,5]:
+    print(i,end=",")#prints:1,22,333,4444,55555
